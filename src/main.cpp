201042d99be2d44ff0da89dd2c5ecace846407d5
Grego1801 <theslngthxngdchchnayus@gmail.com>
2026-03-22 20:46:43 +0700

    use m_attemptTime * 240 for frame

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,8 @@
 using namespace geode::prelude;
 
 #define FRAME_FILE "/sdcard/Android/media/com.geode.launcher/game/gd_frame.txt"
-
-static int g_frame = 0;
 static bool g_playing = false;
+static int g_lastFrame = -1;
 
 void writeFrame(int f) {
     FILE* file = fopen(FRAME_FILE, "w");
@@ -18,18 +17,28 @@ void writeFrame(int f) {
 class $modify(GJBaseGameLayer) {
     void processCommands(float dt, bool isHalfTick, bool isLastTick) {
         GJBaseGameLayer::processCommands(dt, isHalfTick, isLastTick);
-        // Only count on last tick to avoid double counting
-        if (g_playing && isLastTick) {
-            g_frame++;
-            writeFrame(g_frame);
+        if (!g_playing || !isLastTick || isHalfTick) return;
+        
+        auto pl = PlayLayer::get();
+        if (!pl) return;
+        
+        // m_currentTime is elapsed seconds, multiply by TPS to get frame
+        // Use m_gameState.m_unk1 or find correct field
+        // For now derive from attempt time
+        double t = pl->m_attemptTime;
+        int frame = (int)round(t * 240.0);
+        
+        if (frame != g_lastFrame) {
+            g_lastFrame = frame;
+            writeFrame(frame);
         }
     }
 };
 
 class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-        g_frame = 0;
         g_playing = false;
+        g_lastFrame = -1;
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
         g_playing = true;
@@ -38,14 +47,13 @@ class $modify(PlayLayer) {
     }
 
     void resetLevel() {
-        g_frame = 0;
+        g_lastFrame = -1;
         PlayLayer::resetLevel();
         writeFrame(0);
     }
 
     void onQuit() {
         g_playing = false;
-        g_frame = 0;
         writeFrame(-1);
         PlayLayer::onQuit();
     }
