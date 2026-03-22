37c8873400397a0774342c6352de976c682140fa
Grego1801 <theslngthxngdchchnayus@gmail.com>
2026-03-23 01:35:15 +0700

    write to /tmp/gd_frame - RAM tmpfs

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,16 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <cstdio>
 
 using namespace geode::prelude;
 
-#define SHM_NAME "/gd_frame"
-
+#define FRAME_FILE "/tmp/gd_frame"
 static bool g_playing = false;
-static int* g_frame_ptr = nullptr;
-
-void initShm() {
-    int fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
-    if (fd < 0) return;
-    ftruncate(fd, sizeof(int));
-    g_frame_ptr = (int*)mmap(NULL, sizeof(int),
-        PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    close(fd);
-    if (g_frame_ptr) *g_frame_ptr = -1;
-}
 
 void writeFrame(int f) {
-    if (g_frame_ptr) *g_frame_ptr = f;
+    FILE* file = fopen(FRAME_FILE, "w");
+    if (file) { fprintf(file, "%d", f); fclose(file); }
 }
 
 class $modify(GJBaseGameLayer) {
@@ -40,7 +26,6 @@ class $modify(GJBaseGameLayer) {
 
 class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-        if (!g_frame_ptr) initShm();
         g_playing = false;
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
@@ -48,12 +33,10 @@ class $modify(PlayLayer) {
         writeFrame(0);
         return true;
     }
-
     void resetLevel() {
         PlayLayer::resetLevel();
         writeFrame(0);
     }
-
     void onQuit() {
         g_playing = false;
         writeFrame(-1);
