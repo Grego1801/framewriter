6d068847a26c59c3186cb6e83f79cb81b6c318a6
Grego1801 <theslngthxngdchchnayus@gmail.com>
2026-03-22 20:01:46 +0700

    simple frame counter every frame no eclipse dep

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,35 @@
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
 #include <fstream>
-#include <eclipse.hpp>
 
 using namespace geode::prelude;
 
 #define FRAME_FILE "/sdcard/Android/media/com.geode.launcher/game/gd_frame.txt"
+
+static int g_frame = 0;
 static bool g_playing = false;
 
 void writeFrame(int f) {
-    std::ofstream file(FRAME_FILE, std::ios::trunc);
-    if (file.is_open()) file << f;
+    FILE* file = fopen(FRAME_FILE, "w");
+    if (file) {
+        fprintf(file, "%d", f);
+        fclose(file);
+    }
 }
 
 class $modify(GJBaseGameLayer) {
     void update(float dt) {
         GJBaseGameLayer::update(dt);
         if (g_playing) {
-            auto result = eclipse::label::getVariable<int64_t>("frameReal");
-            if (result) {
-                writeFrame((int)result.unwrap());
-            }
+            g_frame++;
+            writeFrame(g_frame);
         }
     }
 };
 
 class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        g_frame = 0;
         g_playing = false;
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
@@ -37,12 +40,14 @@ class $modify(PlayLayer) {
     }
 
     void resetLevel() {
+        g_frame = 0;
         PlayLayer::resetLevel();
         writeFrame(0);
     }
 
     void onQuit() {
         g_playing = false;
+        g_frame = 0;
         writeFrame(-1);
         PlayLayer::onQuit();
     }
