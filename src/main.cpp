1ee743bda7543c2c93c868c9bc9c852a0d1ab299
Grego1801 <theslngthxngdchchnayus@gmail.com>
2026-03-22 20:21:18 +0700

    hook processCommands once per physics frame

--- src/main.cpp
+++ src/main.cpp
@@ -6,35 +6,28 @@
 using namespace geode::prelude;
 
 #define FRAME_FILE "/sdcard/Android/media/com.geode.launcher/game/gd_frame.txt"
-#define PHYSICS_FPS 240.0f
 
-static double g_time = 0.0;
+static int g_frame = 0;
 static bool g_playing = false;
 
 void writeFrame(int f) {
     FILE* file = fopen(FRAME_FILE, "w");
-    if (file) {
-        fprintf(file, "%d", f);
-        fclose(file);
-    }
+    if (file) { fprintf(file, "%d", f); fclose(file); }
 }
 
 class $modify(GJBaseGameLayer) {
-    void update(float dt) {
-        GJBaseGameLayer::update(dt);
+    void processCommands(float dt) {
+        GJBaseGameLayer::processCommands(dt);
         if (g_playing) {
-            // dt is real elapsed time per update
-            // multiply by speed to get physics time
-            g_time += dt;
-            int frame = (int)(g_time * PHYSICS_FPS);
-            writeFrame(frame);
+            g_frame++;
+            writeFrame(g_frame);
         }
     }
 };
 
 class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
-        g_time = 0.0;
+        g_frame = 0;
         g_playing = false;
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
@@ -44,14 +37,14 @@ class $modify(PlayLayer) {
     }
 
     void resetLevel() {
-        g_time = 0.0;
+        g_frame = 0;
         PlayLayer::resetLevel();
         writeFrame(0);
     }
 
     void onQuit() {
         g_playing = false;
-        g_time = 0.0;
+        g_frame = 0;
         writeFrame(-1);
         PlayLayer::onQuit();
     }
