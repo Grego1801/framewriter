ad354bc2708693b7a2107cd85ab1bd87d74960f8
Grego1801 <theslngthxngdchchnayus@gmail.com>
2026-03-23 01:20:51 +0700

    use shared memory instead of file

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,37 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 #include <Geode/modify/GJBaseGameLayer.hpp>
-#include <fstream>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 using namespace geode::prelude;
 
-#define FRAME_FILE "/sdcard/Android/media/com.geode.launcher/game/gd_frame.txt"
+#define SHM_NAME "/gd_frame"
+
 static bool g_playing = false;
+static int* g_frame_ptr = nullptr;
+
+void initShm() {
+    int fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+    if (fd < 0) return;
+    ftruncate(fd, sizeof(int));
+    g_frame_ptr = (int*)mmap(NULL, sizeof(int),
+        PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    close(fd);
+    *g_frame_ptr = -1;
+}
 
 void writeFrame(int f) {
-    FILE* file = fopen(FRAME_FILE, "w");
-    if (file) { fprintf(file, "%d", f); fclose(file); }
+    if (g_frame_ptr) *g_frame_ptr = f;
 }
 
 class $modify(GJBaseGameLayer) {
     void processCommands(float dt, bool isHalfTick, bool isLastTick) {
         GJBaseGameLayer::processCommands(dt, isHalfTick, isLastTick);
         if (!g_playing) return;
-        
         auto pl = PlayLayer::get();
         if (!pl) return;
-        
         int frame = (int)round(pl->m_attemptTime * 480.0);
         writeFrame(frame);
     }
@@ -28,6 +39,7 @@ class $modify(GJBaseGameLayer) {
 
 class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        if (!g_frame_ptr) initShm();
         g_playing = false;
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
