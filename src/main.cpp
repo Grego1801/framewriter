ac93d8d021540876fddee779cb8b5f7fb5c5c38b
Grego1801 <theslngthxngdchchnayus@gmail.com>
2026-03-22 18:54:46 +0700

    hook GJBaseGameLayer update instead

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,42 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/GJBaseGameLayer.hpp>
 #include <fstream>
-#include <string>
 
 using namespace geode::prelude;
 
 #define FRAME_FILE "/sdcard/Android/media/com.geode.launcher/game/gd_frame.txt"
 
 static int g_frame = 0;
+static bool g_playing = false;
 
-class $modify(PlayLayer) {
+class $modify(GJBaseGameLayer) {
     void update(float dt) {
-        PlayLayer::update(dt);
-        g_frame++;
-        
-        if (g_frame % 4 == 0) {
-            std::ofstream f(FRAME_FILE, std::ios::trunc);
-            if (f.is_open()) {
-                f << g_frame;
+        GJBaseGameLayer::update(dt);
+        if (g_playing) {
+            g_frame++;
+            if (g_frame % 4 == 0) {
+                std::ofstream f(FRAME_FILE, std::ios::trunc);
+                if (f.is_open()) f << g_frame;
             }
         }
     }
-    
+};
+
+class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         g_frame = 0;
+        g_playing = false;
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
-        
-        // Write 0 to signal level started
+        g_playing = true;
         std::ofstream f(FRAME_FILE, std::ios::trunc);
         if (f.is_open()) f << 0;
-        
         return true;
     }
-    
+
     void onQuit() {
+        g_playing = false;
         g_frame = 0;
         std::ofstream f(FRAME_FILE, std::ios::trunc);
         if (f.is_open()) f << -1;
