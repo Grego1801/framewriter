bba75a95c35f58389a0ed2ba120dda3413ee5dbd
Grego1801 <theslngthxngdchchnayus@gmail.com>
2026-03-22 18:35:42 +0700

    count frames manually no m_time needed

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,41 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 #include <fstream>
+#include <string>
 
 using namespace geode::prelude;
 
-// Write to Geode's own directory which GD has write access to
 #define FRAME_FILE "/sdcard/Android/media/com.geode.launcher/game/gd_frame.txt"
+
+static int g_frame = 0;
 
 class $modify(PlayLayer) {
     void update(float dt) {
         PlayLayer::update(dt);
+        g_frame++;
         
-        float time = this->m_gameState.m_levelTime;
-        int frame = (int)(time * 240.0f);
-        
-        if (frame % 4 == 0) {
+        if (g_frame % 4 == 0) {
             std::ofstream f(FRAME_FILE, std::ios::trunc);
             if (f.is_open()) {
-                f << frame;
+                f << g_frame;
             }
         }
     }
     
+    bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
+        g_frame = 0;
+        if (!PlayLayer::init(level, useReplay, dontCreateObjects))
+            return false;
+        
+        // Write 0 to signal level started
+        std::ofstream f(FRAME_FILE, std::ios::trunc);
+        if (f.is_open()) f << 0;
+        
+        return true;
+    }
+    
     void onQuit() {
+        g_frame = 0;
         std::ofstream f(FRAME_FILE, std::ios::trunc);
         if (f.is_open()) f << -1;
         PlayLayer::onQuit();
