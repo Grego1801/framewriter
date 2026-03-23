74364330d9289d60d2feb6489bf07ce432346637
Grego1801 <theslngthxngdchchnayus@gmail.com>
2026-03-23 17:34:56 +0700

    fix dead state only written once

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,8 @@ using namespace geode::prelude;
 
 #define STATE_FILE "/sdcard/Android/media/com.geode.launcher/game/gd_state.txt"
 
+static bool g_dead = false;
+
 void writeState(const char* state) {
     int fd = open(STATE_FILE, O_WRONLY | O_CREAT | O_TRUNC, 0666);
     if (fd >= 0) {
@@ -19,21 +21,27 @@ class $modify(PlayLayer) {
     bool init(GJGameLevel* level, bool useReplay, bool dontCreateObjects) {
         if (!PlayLayer::init(level, useReplay, dontCreateObjects))
             return false;
+        g_dead = false;
         writeState("0");
         return true;
     }
 
     void resetLevel() {
+        g_dead = false;
         PlayLayer::resetLevel();
         writeState("0");
     }
 
     void destroyPlayer(PlayerObject* p, GameObject* o) {
         PlayLayer::destroyPlayer(p, o);
-        writeState("1");
+        if (!g_dead) {
+            g_dead = true;
+            writeState("1");
+        }
     }
 
     void onQuit() {
+        g_dead = false;
         writeState("2");
         PlayLayer::onQuit();
     }
